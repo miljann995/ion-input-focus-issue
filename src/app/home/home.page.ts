f0ad84788e6b0c3dd450c7568fc420617c8d7a44
Miljan Veljović <m.veljovic@snapaddy.com>
2022-02-18 12:40:52 +0100

    show ion-toast and ion-loading

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(
+    private readonly toastController: ToastController,
+    private readonly loadingController: LoadingController
+  ) {}
 
-  constructor() {}
+  public async login(): Promise<void> {
+    await this.showLoading();
+    await this.wait();
+    await this.hideLoading();
+    await this.showToast();
+  }
 
+  private async showToast(): Promise<void> {
+    const toast = await this.toastController.create({
+      message: 'Invalid credentials.',
+      color: 'danger',
+    });
+
+    await toast.present();
+  }
+
+  private async showLoading(): Promise<void> {
+    const loading = await this.loadingController.create({
+      message: 'Signing in...',
+    });
+
+    await loading.present();
+  }
+
+  private async hideLoading(): Promise<void> {
+    await this.loadingController.dismiss();
+  }
+
+  private async wait(ms = 1000): Promise<void> {
+    await new Promise((resolve) => setTimeout(resolve, ms));
+  }
 }
